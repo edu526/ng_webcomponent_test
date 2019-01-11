70f90c5d8857865462e80d24f803b2fc027b30de
Eduardo De la Cruz <eduardo@multiplica.com>
2019-01-11 17:26:12 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-chile-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dog-of-the-day';
+}
